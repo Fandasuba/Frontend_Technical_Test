34c5f8c7f3255a280d86dae85dd4b0c83b793ac7
Craig <craigjrobinson@hotmail.co.uk>
2025-01-31 18:04:29 +0000

    Added css for mobile adjustment. landscape and potrait mode have pros and cons.

--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -3,7 +3,6 @@ import podNames from 'ember-component-css/pod-names';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-// import styles from './styles.scss';
 
 export default class MovieListItem extends Component {
   @service firebase;
@@ -13,8 +12,8 @@ export default class MovieListItem extends Component {
   @tracked editDescription;
   @service tmdb;
   @tracked trailerUrl = null;
+
   styleNamespace = podNames['movie-list/movie-list-item'];
-  // styles = styles;
 
   constructor() {
     super(...arguments);
